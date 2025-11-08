86bc9921f40b1ce100409637fbcf4b830cf9306f
m stevens <bomaltennishal@outlook.com>
2025-11-08 16:59:22 +0100

    nieuw

---
+++ src/main/java/org/matthijs/spring_ai_4real/SpringAi4realApplication.java
@@ -0,0 +1,13 @@
+package org.matthijs.spring_ai_4real;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringAi4realApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringAi4realApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/matthijs/spring_ai_4real/SpringAi4realApplicationTests.java
@@ -0,0 +1,13 @@
+package org.matthijs.spring_ai_4real;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringAi4realApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
