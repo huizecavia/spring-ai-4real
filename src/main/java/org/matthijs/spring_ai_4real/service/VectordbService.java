36e22272f2d29e0c27edcfc1f9e9032b06db92df
m stevens <bomaltennishal@outlook.com>
2025-11-29 13:10:40 +0100

    vectorstore

--- src/main/java/org/matthijs/spring_ai_4real/service/VectordbService.java
+++ src/main/java/org/matthijs/spring_ai_4real/service/VectordbService.java
@@ -3,9 +3,9 @@ package org.matthijs.spring_ai_4real.service;
 import org.springframework.ai.document.Document;
 import org.springframework.ai.embedding.EmbeddingModel;
 import org.springframework.ai.vectorstore.SimpleVectorStore;
-import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.stereotype.Service;
 
+import java.io.File;
 import java.util.List;
 
 @Service
@@ -18,16 +18,6 @@ public class VectordbService {
     }
 
 
-//    https://docs.spring.io/spring-ai/reference/api/vectordbs.html
-//    @Autowired
-//    VectorStore vectorStore;
-//
-//    void load(String sourceFile) {
-//        JsonReader jsonReader = new JsonReader(new FileSystemResource(sourceFile),
-//                "price", "name", "shortDescription", "description", "tags");
-//        List<Document> documents = jsonReader.get();
-//        this.vectorStore.add(documents);
-//    }
 
 
     public String getVectorRespons() {
@@ -38,10 +28,13 @@ public class VectordbService {
         String filename = "1-71.txt";
         String abspath = root + filepath + filename;
         Document d = new Document(abspath);
-        embedding.embed(d);
+        List<Document> dlist = List.of(d);
 
-
+        var b = embedding.embed(d);
         SimpleVectorStore vs = SimpleVectorStore.builder(embedding).build();
+        vs.add(dlist);
+        vs.save(new File("vectordb.db"));
+
         return "vectordb";
     }
 }
